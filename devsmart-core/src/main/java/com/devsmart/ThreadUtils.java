2b27540385a9f8a3b75b92bbeeca5bdd2c9a89c9
Paul Soucy <paul@dev-smart.com>
2015-03-11 23:15:33 -0400

    added unhandled exeception handler to ThreadUtils

--- devsmart-core/src/main/java/com/devsmart/ThreadUtils.java
+++ devsmart-core/src/main/java/com/devsmart/ThreadUtils.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ThreadFactory;
 
 public class ThreadUtils {
 
+    public static Thread.UncaughtExceptionHandler CPUUncaughtHandler = null;
     private static final ThreadGroup CPUThreadGroup = new ThreadGroup("CPU Threads");
     private static ThreadFactory CPUThreadPoolFactory = new ThreadFactory() {
 
@@ -16,10 +17,14 @@ public class ThreadUtils {
         @Override
         public Thread newThread(Runnable runnable) {
             Thread retval = new Thread(CPUThreadGroup, runnable, String.format("CPU Thread %d", mThreadNum++));
+            if(CPUUncaughtHandler != null) {
+                retval.setUncaughtExceptionHandler(CPUUncaughtHandler);
+            }
             return retval;
         }
     };
 
+    public static Thread.UncaughtExceptionHandler IOUncaughtHandler = null;
     private static final ThreadGroup IOThreadGroup = new ThreadGroup("IO Threads");
     private static ThreadFactory IOThreadPoolFactory = new ThreadFactory() {
 
@@ -28,6 +33,9 @@ public class ThreadUtils {
         @Override
         public Thread newThread(Runnable r) {
             Thread thread = new Thread(IOThreadGroup, r, String.format("IO Thread %d", mIOThreadNum++));
+            if(IOUncaughtHandler != null) {
+                thread.setUncaughtExceptionHandler(IOUncaughtHandler);
+            }
             return thread;
         }
     };

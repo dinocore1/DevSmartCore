3763821df8c7fa14422f1b43372d65bdeb5b25fc
paul <psoucy@sciaps.com>
2016-08-29 12:35:20 -0400

    fixed uncessary dependency on android support v4

--- devsmart-android/src/main/java/com/devsmart/android/fragments/ChooseFileDialogFragment.java
+++ devsmart-android/src/main/java/com/devsmart/android/fragments/ChooseFileDialogFragment.java
@@ -3,10 +3,10 @@ package com.devsmart.android.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
